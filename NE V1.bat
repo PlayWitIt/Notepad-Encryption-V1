7f133fad0273c9b05d29fc1e007f2514401c432a
PlayWitIt <PlayWitItMyWay@gmail.com>
2024-09-04 21:39:58 -0400

    First working Template of Note Encrypter V1

---
+++ NE V1.bat
@@ -0,0 +1,3 @@
+@echo off
+REM Open a new Command Prompt window and run the Python script
+start cmd /k python "C:\Users\GreyAllDayGrey\OneDrive\Saved Code\PlayWitIt Code\Virtual_Pet V1\Virtual_Pet-V1\#Private_Noteppad.py"
